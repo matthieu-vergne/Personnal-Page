d4f53b11975b4bf6bb87003ef9a63b22e9feb1d9
Sazaju HITOKAGE <sazaju@gmail.com>
2012-10-27 20:16:51 +0200

    Add LinkedIn link.

--- content/home.php
+++ content/home.php
@@ -6,11 +6,13 @@ $data = array(
 	'Nationality' => 'French',
 	'E-mail' => Format::toHtmlEmail('matthieu.vergne@gmail.com'),
 	'Google+' => Format::toHtmlUrl('https://plus.google.com/113987650579242368833'),
+	'LinkedIn' => Format::toHtmlUrl('http://www.linkedin.com/pub/matthieu-vergne/41/832/bb8'),
 	'GitHub' => Format::toHtmlUrl('https://github.com/matthieu-vergne'),
 	'Interests' => 'programming, optimization, AI, mathematics, japanimation',
 	'Skills' => 'Object Oriented programming, optimization techniques, project management',
 	'Current work' => 'PhD. student in Requirements Engineering, Fondazione Bruno Kessler (Trento, Italy)',
 );
+
 foreach($data as $type => $value) {
 	echo "<b>$type:</b> $value<br/>";
 }
