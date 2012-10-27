baa0d30cfa99213176b40be2085aecb620a9a8b6
Sazaju HITOKAGE <sazaju@gmail.com>
2012-10-27 20:07:05 +0200

    Display the last update time.

--- content.php
+++ content.php
@@ -4,5 +4,6 @@ $getFileForPage = function($page) {return "content/$page.php";};
 $default = 'home';
 $file = $getFileForPage(isset($_GET['page']) ? $_GET['page'] : $default);
 $file = is_file($file) ? $file : $getFileForPage($default);
+echo '<div id="lastUpdateTime">Last update: '.date ("d/m/Y H:i:s", filemtime($file)).'</div>';
 include_once($file);
 ?>
