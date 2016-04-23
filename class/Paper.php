9a46436450c24d5dc9f4f78c240d9ca02bc6004d
Matthieu Vergne <matthieu.vergne@gmail.com>
2016-04-23 17:49:56 +0200

    Extract bibliography to dedicated class.

---
+++ class/Paper.php
@@ -0,0 +1,149 @@
+<?php
+class Paper {
+	private $id = null;
+	private $description = null;
+	private $doi = null;
+	private $url = null;
+	private $pdf = null;
+	
+	public function __construct($id) {
+		if (empty($id)) {
+			throw new Exception("No ID provided.");
+		} else {
+			$this->id = $id;
+		}
+	}
+	
+	public function getId() {
+		return $this->id;
+	}
+	
+	public function setDescription($description) {
+		$this->description = $description;
+	}
+	
+	public function getDescription() {
+		return $this->description;
+	}
+	
+	public function setDOI($doi) {
+		$this->doi = $doi;
+	}
+	
+	public function getDOI() {
+		return $this->doi;
+	}
+	
+	public function setURL($url) {
+		$this->url = $url;
+	}
+	
+	public function getURL() {
+		if($this->url != null) {
+			return $this->url;
+		} else if($this->doi != null) {
+			return "http://dx.doi.org/".$this->doi;
+		} else {
+			return null;
+		}
+	}
+	
+	public function setPDF($pdf) {
+		$this->pdf = $pdf;
+	}
+	
+	public function getPDF() {
+		return $this->pdf;
+	}
+	
+	private static $allPapers = null;
+	public static function getAllPapers() {
+		if (Paper::$allPapers === null) {
+			Paper::$allPapers = array();
+			
+			$paper = new Paper('EmpiRE-2012');
+			$paper->setDescription("I. Morales-Ramirez, M. Vergne, M. Morandini, L. Sabatucci, A. Perini, et A. Susi, « Revealing the obvious?: A retrospective artefact analysis for an ambient assisted-living project », in 2012 IEEE Second International Workshop on Empirical Requirements Engineering (EmpiRE), 2012, p. 41 ‑48.");
+			$paper->setDOI("10.1109/EmpiRE.2012.6347681");
+			$paper->setPDF(Resource::getResource(1)->getFileUrl());
+			Paper::$allPapers[] = $paper;
+			
+			$paper = new Paper('RIGiM-2012');
+			$paper->setDescription("I. Morales-Ramirez, M. Vergne, M. Morandini, L. Sabatucci, A. Perini, et A. Susi, « Where Did the Requirements Come from? A Retrospective Case Study », in Advances in Conceptual Modeling, S. Castano, P. Vassiliadis, L. V. Lakshmanan, et M. L. Lee, Éd. Springer Berlin Heidelberg, 2012, p. 185‑194.");
+			$paper->setDOI("10.1007/978-3-642-33999-8_23");
+			$paper->setPDF(Resource::getResource(2)->getFileUrl());
+			Paper::$allPapers[] = $paper;
+			
+			$paper = new Paper('iStar-2013');
+			$paper->setDescription("M. Vergne, I. Morales-Ramirez, M. Morandini, A. Susi, et A. Perini, « Analysing User Feedback and Finding Experts: Can Goal-Orientation Help? », in 6th International i* Workshop, Valencia, Spain, 2013, vol. 978, p. 49‑54.");
+			$paper->setURL("http://ceur-ws.org/Vol-978/paper_9.pdf");
+			$paper->setPDF("http://ceur-ws.org/Vol-978/paper_9.pdf");
+			Paper::$allPapers[] = $paper;
+			
+			$paper = new Paper('ICSE-2014');
+			$paper->setDescription("I. Morales-Ramirez, M. Vergne, M. Morandini, A. Siena, A. Perini, et A. Susi, « Who is the Expert? Combining Intention and Knowledge of Online Discussants in Collaborative RE Tasks », in Companion Proceedings of the 36th International Conference on Software Engineering, New York, NY, USA, 2014, p. 452–455.");
+			$paper->setDOI("10.1145/2591062.2591103");
+			$paper->setPDF("http://dl.acm.org/authorize?N08506");
+			Paper::$allPapers[] = $paper;
+			
+			$paper = new Paper('CAISE-2014');
+			$paper->setDescription("M. Vergne et A. Susi, « Expert Finding Using Markov Networks in Open Source Communities », in Advanced Information Systems Engineering, M. Jarke, J. Mylopoulos, C. Quix, C. Rolland, Y. Manolopoulos, H. Mouratidis, et J. Horkoff, Éd. Springer International Publishing, 2014, p. 196‑210.");
+			$paper->setDOI("10.1007/978-3-319-07881-6_14");
+			$paper->setPDF(Resource::getResource(3)->getFileUrl());
+			Paper::$allPapers[] = $paper;
+			
+			$paper = new Paper('EvoSoft-2014');
+			$paper->setDescription("A. J. Nebro, J. J. Durillo, et M. Vergne, « Redesigning the jMetal Multi-Objective Optimization Framework », in Companion Publication of the 2015 Annual Conference on Genetic and Evolutionary Computation, 2015, p. 1093‑1100.");
+			$paper->setDOI("10.1145/2739482.2768462");
+			$paper->setPDF("http://dl.acm.org/authorize?N08507");
+			Paper::$allPapers[] = $paper;
+			
+			$paper = new Paper('ER-2015');
+			$paper->setDescription("M. Vergne et A. Susi, « Breaking the Recursivity: Towards a Model to Analyse Expert Finders », in Conceptual Modeling, vol. 9381, P. Johannesson, M. L. Lee, S. W. Liddle, A. L. Opdahl, et Ó. P. López, Éd. Cham: Springer International Publishing, 2015, p. 539‑547.");
+			$paper->setDOI("10.1007/978-3-319-25264-3_40");
+			$paper->setPDF(Resource::getResource(4)->getFileUrl());
+			Paper::$allPapers[] = $paper;
+			
+			// Check unique IDs
+			$ids = array();
+			foreach(Paper::$allPapers as $paper) {
+				$id = $paper->getID();
+				if (isset($ids[$id])) {
+					throw new Exception($id." used more than once.");
+				} else {
+					$ids[$id] = $paper;
+				}
+			}
+			
+			// Check provide descriptions
+			foreach(Paper::$allPapers as $paper) {
+				if (empty($paper->getDescription())) {
+					throw new Exception("No description provided for paper ".$paper->getID());
+				} else {
+					// OK
+				}
+			}
+			
+			// Check provide URL
+			foreach(Paper::$allPapers as $paper) {
+				if (empty($paper->getURL())) {
+					throw new Exception("No URL provided for paper ".$paper->getID());
+				} else {
+					// OK
+				}
+			}
+		}
+		return Paper::$allPapers;
+	}
+	
+	public static function getPaper($id) {
+		foreach(Paper::getAllPapers() as $paper) {
+			if ($paper->getID() === $id) {
+				return $paper;
+			} else {
+				// Not found yet
+			}
+		}
+		throw new Exception($id." is not a known paper ID.");
+	}
+}
+?>
