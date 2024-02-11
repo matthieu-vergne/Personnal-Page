483ef41ba38de12154222e707501ff73b436917c
Matthieu Vergne <matthieu.vergne@gmail.com>
2024-02-12 00:20:27 +0100

    Generalize toggle

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
+const toggleMap = new Map();
 function toggle(...ids) {
 	for (var id of ids) {
 		var style = document.getElementById(id).style;
 		if (style.display !== "none") {
+			toggleMap.set(id, style.display);
 			style.display = "none";
 		} else {
-			style.display = "inline-block";
+			style.display = toggleMap.get(id);
 		}
 	}
 }
